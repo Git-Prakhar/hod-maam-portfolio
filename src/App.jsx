3f5308d5052f0c58f62a020843058649301b56dd
Git-Prakhar <kprakhar01@gmail.com>
2024-03-16 23:52:55 +0530

    Removed React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import HomePage from './components/HomePage'
 
 function App() {
   return (
     <>
-      <Router>
-        <Routes>
-          <Route exact path='/' element={<HomePage />}></Route>
-        </Routes>
-      </Router>
+      <HomePage />
     </>
   )
 }
